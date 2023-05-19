74639edab12e5b3ff3c9dcac865eea960a6390cc
koffy <timiakinyem@gmail.com>
2023-05-19 02:59:33 +0100

    W

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 
-
-
 let userInput=document.getElementById('user-input');
 let userAvatar=document.getElementById('avatar');
 let demo=document.getElementById('demo');
+
+function myImage(input){
+    // let selectedImages = userInput.files;
+        var reader = new FileReader();
+        reader.onload=(e)=>{
+            userAvatar.src = e.target.result;
+        };
+    if (input.files[0]){
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+/*
 function myImage(input){
     // let selectedImages = userInput.files;
     if(input.files && input.files[0]){
@@ -14,8 +24,7 @@ function myImage(input){
     reader.readAsDataURL(input.files[0]);
     demo.innerText="You're Welcome";
     }
-}
-
+}*/
 let menuBar = document.getElementById("menu");
 let togglePage = document.getElementById("toggle-page");
 let listElement = document.querySelectorAll("ul li")
